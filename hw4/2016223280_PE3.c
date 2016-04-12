6b54e16a106d9f9b1834c9fe0eef2744de227fd1
ss-2016223280 <yunstar1105@naver.com>
2016-04-12 18:14:47 +0900

    2016223280_PE3

---
+++ hw4/2016223280_PE3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+
+main(int ac,char *av[])
+{
+DIR *dir_ptr;
+int result;
+char A[100];
+if(ac != 3)
+{
+	printf("error!!!");
+	exit(0);
+}
+
+if((dir_ptr = opendir(av[2])) == NULL)
+{
+	result = rename(av[1],av[2]);
+	printf("It renames the file %s into %s\n",av[1],av[2]);
+}
+else 
+{
+
+	strcpy(A,av[2]);
+	strcat(A,"/");
+	strcat(A,av[1]);
+	result = link(av[1],A);
+	int r = unlink(av[1]);
+	printf("It moves the file %s into the directory %s\n",av[1],av[2]);
+
+}
+
+
+}
+
